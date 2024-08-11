c2635209958f96407b552230ee233218d06e50e7
TewodrosAdimas <yosefenadagim@gmail.com>
2024-08-11 17:38:24 +0300

    introduction to django

--- 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
+++ 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
@@ -7,7 +7,7 @@ admin.ModelAdmin.register(Book)
 
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')
+    search_fields = ('title', 'author', 'publication_year')
     list_filter = ("publication_year")
 
 admin.site.register(Book, BookAdmin)
