bda2ea3560dfe6673a9e30df7d4293b9abc2bcb0
TewodrosAdimas <yosefenadagim@gmail.com>
2024-08-23 21:27:39 +0300

    model setup

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,8 +1,13 @@
 # from relationship_app.models import Author
-from .models import Author, Library, Librarian
+from .models import Author, Library, Librarian, Book
 
-book = Author.objects.filter(name = "bealu")
+author = Author.objects.get(name="John Doe")
+books_by_author = Book.objects.filter(author=author)
+
 all_books = Library.objects.all()
 all_librarian = Librarian.objects.all()
 for librarian in all_librarian:
     print (librarian.name)
+
+    # relationship_app/query_samples.py
+    # doesn't contain: ["Library.objects.get(name=library_name)", "books.all()"]
