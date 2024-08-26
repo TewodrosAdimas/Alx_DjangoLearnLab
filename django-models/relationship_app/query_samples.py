ba5c9fe92855d38915ec1ea23403b7883ea533e3
TewodrosAdimas <yosefenadagim@gmail.com>
2024-08-26 21:46:48 +0300

    sample query corrected

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -9,7 +9,7 @@ library_name = "Central Library"
 books_in_library = Library.objects.get(name=library_name).books.all()
 
 
-librarian = "Jane Smith"
-librarian = Library.objects.get(library=librarian)
+library = "Jane Smith"
+librarian = Library.objects.get(library=library)
 
 # Author.objects.get(name=author_name)
