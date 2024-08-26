88cade9474acc5af789ea29dc72ff0b428548129
TewodrosAdimas <yosefenadagim@gmail.com>
2024-08-26 23:51:45 +0300

    user authentication corrected

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,39 +1,24 @@
-from typing import Any
 from django.shortcuts import render
 from .models import Librarian, Book, Author
 from .models import Library
 from django.views.generic import ListView, DetailView
-from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
-from django.contrib.auth.views import LoginView, LogoutView
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import logout as auth_logout
 
+def custom_login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user) 
+            return redirect('home')  
+    else:
+        form = AuthenticationForm()
+    
+    return render(request, 'login.html', {'form': form})
 
-
-def BookList(request):
-    all_books = Book.objects.all()
-    return render(request, "relationship_app/list_books.html", all_books)
-class ListAllBooks(ListView):
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'books'  # Name to use in the template
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        library_name = "Central Library"
-        context['library'] = Library.objects.get(name=library_name)
-        # library = Library.objects.get(name=library_name)
-        # context['books'] = library.books.all()       
-        return context
-
-
-class CustomLoginView(LoginView):
-    template_name = 'login.html'
-
-class CustomLogoutView(LogoutView):
-    template_name = 'logout.html'
-
-class RegisterView(CreateView):
-    form_class = UserCreationForm
-    template_name = 'register.html'
-    success_url = reverse_lazy('login')
+def custom_logout_view(request):
+    auth_logout(request)  
+    return redirect('login')  
