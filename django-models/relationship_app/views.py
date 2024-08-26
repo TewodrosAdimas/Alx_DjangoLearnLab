e35e00fc768d0fe8f15bb72634ce84463f8a62ed
TewodrosAdimas <yosefenadagim@gmail.com>
2024-08-26 22:30:57 +0300

    book list view added

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Librarian, Library, Book, Author
 # Create your views here.
+
+def BookList(request):
+    all_books = Book.objects.all()
+    return render(request, "relationship_app/books.html", all_books)
