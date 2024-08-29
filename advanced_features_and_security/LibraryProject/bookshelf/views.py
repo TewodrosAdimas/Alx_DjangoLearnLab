2883a1f1b0f84bd84fab0b71ab39d3bb4b77a3f0
TewodrosAdimas <yosefenadagim@gmail.com>
2024-08-29 11:05:46 +0300

    django permissions

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -8,25 +8,25 @@ from django.core.exceptions import PermissionDenied
 
 # Create your views here.
 @permission_required('your_app_name.can_edit', raise_exception=True)
-def edit_books(request, books_id):
-    books = get_object_or_404(books, id=books_id)
+def edit_books(request, book_id):
+    books = get_object_or_404(books, id=book_id)
     if request.method == 'POST':
         # Process form data and update the books
         form = booksForm(request.POST, instance=books)
         if form.is_valid():
             form.save()
-            return redirect('books_detail', books_id=books.id)  # Redirect to the books detail view
+            return redirect('book_detail', book_id=books.id)  # Redirect to the books detail view
     else:
         form = booksForm(instance=books)
 
     return render(request, 'your_app_name/edit_books.html', {'form': form})
 
 @permission_required('your_app_name.can_delete', raise_exception=True)
-def delete_books(request, books_id):
-    books = get_object_or_404(books, id=books_id)
+def delete_books(request, book_id):
+    books = get_object_or_404(books, id=book_id)
     if request.method == 'POST':
         books.delete()
-        return redirect('books_list')  # Redirect to the books list view after deletion
+        return redirect('book_list')  # Redirect to the books list view after deletion
 
     return render(request, 'your_app_name/delete_books.html', {'books': books})
 
