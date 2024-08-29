e3f3651027481cd644182c7997babdcebff2902e
TewodrosAdimas <yosefenadagim@gmail.com>
2024-08-29 11:40:54 +0300

    security measures

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,4 +1,13 @@
-from django import forms
+# LibraryProject/bookshelf/forms.py
 
-class BookSearchForm(forms.Form):
-    query = forms.CharField(max_length=100, required=False)  # Define validation rules
+from django import forms
+from .models import Book
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author']
+        labels = {
+            'title': 'Book Title',
+            'author': 'Author',
+        }
