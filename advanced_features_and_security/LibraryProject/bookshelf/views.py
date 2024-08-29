4a2fd026329a6cdef6fe34f7f32a8291f06b0aea
TewodrosAdimas <yosefenadagim@gmail.com>
2024-08-29 11:04:12 +0300

    django permissions

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,33 +2,33 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponseForbidden
-from .models import Book
+from .models import books
 from django.core.exceptions import PermissionDenied
 
 
 # Create your views here.
 @permission_required('your_app_name.can_edit', raise_exception=True)
-def edit_book(request, book_id):
-    book = get_object_or_404(Book, id=book_id)
+def edit_books(request, books_id):
+    books = get_object_or_404(books, id=books_id)
     if request.method == 'POST':
-        # Process form data and update the book
-        form = BookForm(request.POST, instance=book)
+        # Process form data and update the books
+        form = booksForm(request.POST, instance=books)
         if form.is_valid():
             form.save()
-            return redirect('book_detail', book_id=book.id)  # Redirect to the book detail view
+            return redirect('books_detail', books_id=books.id)  # Redirect to the books detail view
     else:
-        form = BookForm(instance=book)
+        form = booksForm(instance=books)
 
-    return render(request, 'your_app_name/edit_book.html', {'form': form})
+    return render(request, 'your_app_name/edit_books.html', {'form': form})
 
 @permission_required('your_app_name.can_delete', raise_exception=True)
-def delete_book(request, book_id):
-    book = get_object_or_404(Book, id=book_id)
+def delete_books(request, books_id):
+    books = get_object_or_404(books, id=books_id)
     if request.method == 'POST':
-        book.delete()
-        return redirect('book_list')  # Redirect to the book list view after deletion
+        books.delete()
+        return redirect('books_list')  # Redirect to the books list view after deletion
 
-    return render(request, 'your_app_name/delete_book.html', {'book': book})
+    return render(request, 'your_app_name/delete_books.html', {'books': books})
 
 
 def custom_permission_denied_view(request, exception):
