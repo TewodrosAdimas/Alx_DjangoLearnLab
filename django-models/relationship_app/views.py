a6e2cad7c89ae72409e6eb2dc8bf6ead2fa25522
TewodrosAdimas <yosefenadagim@gmail.com>
2024-08-30 13:42:06 +0300

    funcion and class based  view

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Book
+from .models import Library
 
 # Function-based view to list all books
 def list_books(request):
