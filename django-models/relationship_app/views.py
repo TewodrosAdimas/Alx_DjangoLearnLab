4e3d9ef006d016c249710bd17fd263bb3af569bd
TewodrosAdimas <yosefenadagim@gmail.com>
2024-08-30 13:32:07 +0300

    funcion based view

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Book
 
 def list_books(request):
-    books = Book.objects.all()
-    book_list = "\n".join([f"{book.title} by {book.author.name}" for book in books])
-    return HttpResponse(book_list, content_type="text/plain")
+    books = Book.objects.all()  # Query all books
+    return render(request, 'relationship_app/list_books.html', {'books': books})
