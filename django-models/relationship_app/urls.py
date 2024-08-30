ca66f92ea5c62bebb4eac8201533e92d96d0e734
TewodrosAdimas <yosefenadagim@gmail.com>
2024-08-30 13:39:27 +0300

    funcion based view

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from .views import CustomLoginView, CustomLogoutView, CustomRegisterView
 from .import views
+from .views import list_books, LibraryDetailView
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', CustomLogoutView.as_view(template_name='logout.html'), name='logout'),
     path('register/', CustomRegisterView.as_view(template_name='register.html'), name='register'),
-    path('books/', views.list_books, name='list_books'),
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from .models import Book
 
+from django.urls import path
+from .views import list_books, LibraryDetailView
+
 def list_books(request):
-    books = Book.objects.all()  # Query all books from the database
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+    books = Book.objects.all()
+    return render(request, 'list_books.html', {'books': books})
+
+urlpatterns = [
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+]
