086e03b5fbd29a679b040f0eaf976f8bfbb49b1d
TewodrosAdimas <yosefenadagim@gmail.com>
2024-08-30 13:19:51 +0300

    model modified

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -10,7 +10,7 @@ class Author(models.Model):
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
-    author = models.ForeignKey("Author", on_delete=models.CASCADE, related_name="author")
+    author = models.ForeignKey("Author", on_delete=models.CASCADE, related_name="books")
 
     def __str__(self):
         return self.title
@@ -18,7 +18,7 @@ class Book(models.Model):
 
 class Library(models.Model):
     name = models.CharField(max_length=100)
-    books = models.ManyToManyField("Book",related_name="books")
+    books = models.ManyToManyField("Book",related_name="libraries")
 
     def __str__(self):
         return self.name
