8a2ee742100ff8687cb542bfc0ca1c707df195a3
TewodrosAdimas <yosefenadagim@gmail.com>
2024-08-30 13:45:45 +0300

    funcion and class based  view

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('login/', CustomLoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', CustomLogoutView.as_view(template_name='logout.html'), name='logout'),
     path('register/', CustomRegisterView.as_view(template_name='register.html'), name='register'),
-    path('books/', list_books, name='list_books'),
+     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-
 ]
