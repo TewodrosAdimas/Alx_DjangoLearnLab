d131685f7cb2f64921cce97f119d409015c9df5f
TewodrosAdimas <yosefenadagim@gmail.com>
2024-08-30 13:33:27 +0300

    funcion based view

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-from .models import Book
+from django.shortcuts import render, get_object_or_404
+from .models import Library
 
-def list_books(request):
-    books = Book.objects.all()  # Query all books
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+def library_detail(request, library_id):
+    library = get_object_or_404(Library, id=library_id)
+    return render(request, 'relationship_app/library_detail.html', {'library': library})
